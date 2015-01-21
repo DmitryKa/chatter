d73f4f8a1b548d8cb0c62c29f90f69484d791ea8
Dmitry Kamorin <dkamorin@thumbtack.net>
2015-01-21 14:25:50 +0600

    Build messages on a client side

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -2,11 +2,14 @@ var server_address = 'http://localhost:3000';
 var socket = io.connect(server_address);
 
 socket.on('get history', function (history) {
-    $('#messages').val(history);
+    console.log(history);
+    for(var i in history) {
+        $('#messages').append(buildMessageNode(history[i]));
+    }
 });
 
 socket.on('update history', function (message) {
-    $('#messages').val($('#messages').val() + message);
+    $('#messages').append(buildMessageNode(message));
 })
 
 function send_by_enter(event) {
@@ -19,4 +22,8 @@ function send_message() {
     var message = $('#newMessage').val();
     socket.emit('new message', message );
     $('#newMessage').val('');
+}
+
+function buildMessageNode(message) {
+    return '<div><span>' + message.name + ':</span> ' + message.message + '</div>';
 }
