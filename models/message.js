3ecfc7c9e0ac8f8a2b4b3973755bfda117e8fd6a
Dmitry Kamorin <dkamorin@thumbtack.net>
2015-01-23 19:03:42 +0600

    Make it capped

--- models/message.js
+++ models/message.js
@@ -1,11 +1,18 @@
 var mongoose = require('../db');
 var Schema = mongoose.Schema;
+var config = require('../config');
 
 var messageSchema = new Schema({
-    name: String,
-    text: String,
-    date: { type: Date, default: Date.now }
-});
+        name: String,
+        text: String,
+        date: { type: Date, default: Date.now }
+    },
+    {
+        capped: {
+            size: config.get('schema:sizeInBytes'),
+            max: config.get('schema:maxDocsNumber')
+        }
+    });
 
 var Message = mongoose.model('Message', messageSchema);
 
