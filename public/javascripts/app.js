831cac399901f191a04f701eaad5a9e0c9e8e634
Dmitry Kamorin <dkamorin@thumbtack.net>
2015-02-03 17:37:01 +0600

    Scroll to bottom when new message appears

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,18 +2,24 @@ var server_address = location.href;
 var socket = io.connect(server_address);
 
 socket.on('get history', function (history) {
+    var messages = $('#messages');
     for(var i in history) {
-        $('#messages').append(buildCommonMessageNode(history[i]));
+        messages.append(buildCommonMessageNode(history[i]));
     }
+    scrollToBottom(messages);
 });
 
 socket.on('common message', function (message) {
-    $('#messages').append(buildCommonMessageNode(message));
-})
+    var messages = $('#messages');
+    messages.append(buildCommonMessageNode(message));
+    scrollToBottom(messages);
+});
 
 socket.on('system message', function(message) {
-    $('#messages').append(buildSystemMessageNode(message));
-})
+    var messages = $('#messages');
+    messages.append(buildSystemMessageNode(message));
+    scrollToBottom(messages);
+});
 
 function sendByEnter(event) {
     if (event.keyCode == 13) {
@@ -35,4 +41,8 @@ function buildCommonMessageNode(message) {
 
 function buildSystemMessageNode(message) {
     return '<div class="sys-msg">' + message.text + '</div>';
+}
+
+function scrollToBottom(element) {
+    element.animate({ scrollTop: element.height()}, 'slow');
 }
